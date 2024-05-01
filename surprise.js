f13e46cdd4c7e00944ecabde4232bf95686df135
lannicollene <57849492+lannicollene@users.noreply.github.com>
2024-04-30 21:37:38 -1000

    moved js code out of html and revised working js code in surprise.js

--- surprise.js
+++ surprise.js
@@ -1,71 +1,61 @@
-
-
 let random_images_array = [{
-  img: "images/spirited-away.jpg",
+  img: "ICS200-Final-Project/images/spirited-away.jpg",
   link: "April.html"
 },
 {
-  img: "images/Nausicaa.jpg",
+  img: "ICS200-Final-Project/images/Nausicaa.jpg",
   link: "May.html#Nausicaa"
 },
 {
-  img: "images/Castle.jpg",
+  img: "ICS200-Final-Project/images/Castle.jpg",
   link: "May.html#Castle"
 }, 
 {
-  img: "images/Arrietty.jpg",
+  img: "ICS200-Final-Project/images/Arrietty.jpg",
   link: "June.html#Arrietty"
 },
 {
-  img: "images/Marnie.jpg",
+  img: "ICS200-Final-Project/images/Marnie.jpg",
   link: "June.html#Marnie"
 },
 {
-  img: "images/Mononoke.jpg",
+  img: "ICS200-Final-Project/images/Mononoke.jpg",
   link: "July.html#Mononoke"
 },
 {
-  img: "images/Ponyo.jpg",
+  img: "ICS200-Final-Project/images/Ponyo.jpg",
   link: "August.html#Ponyo"
 },
 {
-  img: "images/Whisper.jpg",
+  img: "ICS200-Final-Project/images/Whisper.jpg",
   link: "August.html#Whisper"
 },
 {
-  img: "images/CatReturns.jpg",
+  img: "ICS200-Final-Project/images/CatReturns.jpg",
   link: "August.html#Cat"
 },
 {
-  img: "images/Howls.jpg",
+  img: "ICS200-Final-Project/images/Howls.jpg",
   link: "September.html#Howls"
 },
 {
-  img: "images/Kiki.jpg",
+  img: "ICS200-Final-Project/images/Kiki.jpg",
   link: "October.html#Kiki"
 },
 {
-  img: "images/PomPoko.jpg",
+  img: "ICS200-Final-Project/images/PomPoko.jpg",
   link: "November.html#PomPoko"
 },
 {
-  img: "images/Kaguya.jpg",
+  img: "ICS200-Final-Project/images/Kaguya.jpg",
   link: "November.html#Kaguya"
 },
 {
-  img: "images/Totoro.jpg",
+  img: "ICS200-Final-Project/images/Totoro.jpg",
   link: "December.html#Totoro"
 }
 ];
 
-//function getRandomImage(imgAr, path) {
-//  path = path || 'images/';
-//  let num = Math.floor(Math.random() * imgAr.length);
- // let img = path + imgAr[num].img;
- // console.log()
- // let imgStr = `<a href='${imgAr[num].link}'><img src="${img}" alt="" height="400" border="0" /></a>`;
-// document.querySelector('#img-container').innerHTML = imgStr;
-//}
 
 function getRandomImage(imgAr, path) {
 // Add a slash ("/") at the end of the path if it's missing
