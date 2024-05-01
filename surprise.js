08d21ef40cde4bea0a328c81c1fcb4aadacd0ac0
lannicollene <57849492+lannicollene@users.noreply.github.com>
2024-04-30 20:56:52 -1000

    revised html, css and js code

--- surprise.js
+++ surprise.js
@@ -11,18 +11,71 @@ let random_images_array = [{
 {
   img: "images/Castle.jpg",
   link: "May.html#Castle"
+}, 
+{
+  img: "images/Arrietty.jpg",
+  link: "June.html#Arrietty"
+},
+{
+  img: "images/Marnie.jpg",
+  link: "June.html#Marnie"
+},
+{
+  img: "images/Mononoke.jpg",
+  link: "July.html#Mononoke"
+},
+{
+  img: "images/Ponyo.jpg",
+  link: "August.html#Ponyo"
+},
+{
+  img: "images/Whisper.jpg",
+  link: "August.html#Whisper"
+},
+{
+  img: "images/CatReturns.jpg",
+  link: "August.html#Cat"
+},
+{
+  img: "images/Howls.jpg",
+  link: "September.html#Howls"
+},
+{
+  img: "images/Kiki.jpg",
+  link: "October.html#Kiki"
+},
+{
+  img: "images/PomPoko.jpg",
+  link: "November.html#PomPoko"
+},
+{
+  img: "images/Kaguya.jpg",
+  link: "November.html#Kaguya"
+},
+{
+  img: "images/Totoro.jpg",
+  link: "December.html#Totoro"
 }
 ];
 
+//function getRandomImage(imgAr, path) {
+//  path = path || 'images/';
+//  let num = Math.floor(Math.random() * imgAr.length);
+ // let img = path + imgAr[num].img;
+ // console.log()
+ // let imgStr = `<a href='${imgAr[num].link}'><img src="${img}" alt="" height="400" border="0" /></a>`;
+// document.querySelector('#img-container').innerHTML = imgStr;
+//}
+
 function getRandomImage(imgAr, path) {
-  path = path || 'images/';
-  let num = Math.floor(Math.random() * imgAr.length);
-  let img = path + imgAr[num].img;
-  console.log()
-  let imgStr = `<a href='${imgAr[num].link}'><img src="${img}" alt="" height="400" border="0" /></a>`;
-  document.querySelector('#img-container').innerHTML = imgStr;
+// Add a slash ("/") at the end of the path if it's missing
+path = path.endsWith('/') ? path : path + '/';
+let num = Math.floor(Math.random() * imgAr.length);
+let img = path + imgAr[num].img;
+let imgStr = `<a href='${imgAr[num].link}'><img src="${img}" alt="" height="500" border="0" target="_blank" /></a>`;
+document.querySelector('#image-container').innerHTML = imgStr;
 }
 
 document.getElementById('btn').addEventListener('click', () => {
-  getRandomImage(random_images_array, 'img/img-container')
+  getRandomImage(random_images_array, '/')
 })
